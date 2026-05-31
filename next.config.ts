f89ad3f6b4693abf0a27243aee357d8bbca7c84d
Paul <paul@suedeapple.co.uk>
2026-05-31 20:23:55 +0100

    Fix  image loader configuration  - remove turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
+import type { NextConfig } from 'next'
 
-module.exports = {
+const nextConfig: NextConfig = {
   async headers() {
     return [
       {
         source: "/api/revalidate",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: process.env.UMBRACO_REVALIDATE_ACCESS_CONTROL_ORIGIN },
+          { key: "Access-Control-Allow-Origin", value: process.env.UMBRACO_REVALIDATE_ACCESS_CONTROL_ORIGIN ?? '' },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
         ],
@@ -17,4 +18,7 @@ module.exports = {
     loader: 'custom',
     loaderFile: './src/image-loader.ts',
   },
+
 }
+
+export default nextConfig
